c7aca09ccc0af94242bd8e1fcee21cb46923d712
Reiii3 <mohtoyib79@gmail.com>
2025-02-28 11:06:11 +0700

    update exoins.sh

--- exoins.sh
+++ exoins.sh
@@ -1,1 +1,34 @@
+bin="/data/local/tmp/exogame"
+fun="https://reiii3.github.io/GVRSH/function/function.sh"
+onprop="https://reiii3.github.io/EXOGAME/bin/prop.sh"
+if [ ! -f $bin ]; then
+  mkdir -p "$bin"
+fi
+storm -rP "$bin" -s "${fun}" -fn "function" "$@"
+sleep 1
+storm -rP "$bin" -s "${onprop}" -fn "prop" "$@"
 
+$AXFUN
+import axeron.prop
+. $bin/function
+. $bin/prop
+
+if [ -n "$1" ] && [ "$1" == "-g" ];then
+    axprop $path_online nameGame -s "$2"
+    nameGame="$2"
+    shift 2
+    pkg=$(pm list packages | grep -i "$nameGame" | sed 's/package://g')
+    axprop $path_axeronprop runPackage -s "$pkg"
+    runPackage="$pkg"
+fi
+if [ -n "$1" ] && [ "$1" == "-v" ];then
+    renderer="$2"
+    shift 
+fi
+
+if [ $maintenance = "true" ]; then
+   echo "======================="
+   echo "  EXOGAME 1.0 BETA ON"
+   echo "======================="
+   exit 0
+fi
