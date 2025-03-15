3a40c9ec366d23f35bfdefd5b1261042aee6d690
Reiii3 <mohtoyib79@gmail.com>
2025-03-15 16:00:06 +0700

    update exoins.sh

--- exoins.sh
+++ exoins.sh
@@ -9,9 +9,11 @@ fi
 
 $AXFUN
 import axeron.prop
-storm -rP "$bin" -s "${fun}" -fn "function" "$@"
-sleep 1
-storm -rP "$bin" -s "${onprop}" -fn "prop" "$@"
+if [ ! -f "$prop" ] && [ ! -f "$function" ]; then
+  storm -rP "$bin" -s "${fun}" -fn "function" "$@"
+  sleep 1
+  storm -rP "$bin" -s "${onprop}" -fn "prop" "$@"
+fi
 local cek_id=$(storm "https://reiii3.github.io/EXOGAME/user/beta.txt")
 local cek_beta=$(echo "$cek_id" | grep -q "$AXERONID" && echo true || echo false)
 . $function
