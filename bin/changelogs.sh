d3dd2ce2eb700c403a68cbf771b44d97d349bea4
Reiii3 <mohtoyib79@gmail.com>
2025-03-18 06:49:23 +0700

    update bin/changelogs.sh

--- bin/changelogs.sh
+++ bin/changelogs.sh
@@ -1,5 +1,9 @@
 local bin="/data/local/tmp/exogame"
 local prop="$bin/prop"
+local adjust="[Penyesuaian] :"
+local new="[New] :"
+local des="[Deskripsi] :"
+local war="[Warning] :"
 $AXFUN
 local cek_id=$(storm "https://reiii3.github.io/EXOGAME/user/beta.txt")
 local cek_beta=$(echo "$cek_id" | grep -q "$AXERONID" && echo true || echo false)
@@ -15,7 +19,9 @@ fi
 echo
 if [ $cek_beta = true ]; then
   echo "   ┌[Exo] New Update Beta Version"
-  echo "   └"
+  echo "   └┬$new AI System By Modules Foxver Auto"
+  echo "    ├─$des Ini adalah system yang sudah saya rancang untuk mengaktifkan mode yang sudah anda pilih secara otomatis dan jika anda ingin mengganti Game anda harus mereboot system ai nya"
+  echo "    ├─$war Fitur ini masih dalam tahab pengembangan Jangan gunakan terlalu sering di karenakn saya masih menganalisa kekurangan fitur ini"
 else
   
 fi
