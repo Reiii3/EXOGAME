7d72958246287d240ed64893e81a7f89f624aa9f
Reiii3 <mohtoyib79@gmail.com>
2025-03-18 06:14:58 +0700

    update bin/changelogs.sh

--- bin/changelogs.sh
+++ bin/changelogs.sh
@@ -1,1 +1,14 @@
-
+local bin="/data/local/tmp/exogame"
+local prop="$bin/prop"
+$AXFUN
+local cek_id=$(storm "https://reiii3.github.io/EXOGAME/user/beta.txt")
+local cek_beta=$(echo "$cek_id" | grep -q "$AXERONID" && echo true || echo false)
+import axeron.prop
+. $prop
+echo -n "$name | "
+sleep 0.2
+if [ "$cek_beta" = true ]; then
+   echo "Beta Version $betver"
+else
+   echo "Full Version $ver"
+fi
