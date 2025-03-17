072a6c39b991a56e74101fbd2424201135635a1c
Reiii3 <mohtoyib79@gmail.com>
2025-03-18 06:54:05 +0700

    update bin/changelogs.sh

--- bin/changelogs.sh
+++ bin/changelogs.sh
@@ -20,8 +20,8 @@ echo
 if [ $cek_beta = true ]; then
   echo "   ┌[Exo] New Update Beta Version"
   echo "   └┬$new AI System By Modules Foxver Auto"
-  echo "    ├─$des Ini adalah system yang sudah saya rancang untuk mengaktifkan mode yang sudah anda pilih secara otomatis dan jika anda ingin mengganti Game anda harus mereboot system ai nya"
-  echo "    ├─$war Fitur ini masih dalam tahab pengembangan Jangan gunakan terlalu sering di karenakn saya masih menganalisa kekurangan fitur ini"
+  echo "    ├─$des System pintar yang dapat mengaktikan mode secara otomatis"
+  echo "    ├─$war Masih Dalam pengembangan, "
 else
   echo "Tesss"
 fi
