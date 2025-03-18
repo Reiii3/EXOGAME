bc0a1d2c77d88256d62f0ab080a698baa0552931
Reiii3 <mohtoyib79@gmail.com>
2025-03-18 15:51:32 +0700

    update bin/maintenance.sh

--- bin/maintenance.sh
+++ bin/maintenance.sh
@@ -2,14 +2,14 @@ local new="[New]"
 local tnks="[Copyrigth By ReiiEja]"
 local ads="[Adjustment]"
 echo "Exo Game | Maintenance
-       [Exo] Add New Feature && Adjustment
-         ┌$new AOT Compiler
-         │Ini Adalah fitur compiler yang lebih optimal
-         ├$new AI System By Foxver Auto
-         │Ini adalah system ai yang di rancang untuk
-         │mengaktifkan auto render atau bisa di sebut
-         │mengubah render saat di dalam game Untuk aktivasi
-         │ada di changelogs
-         └┬$ads Menyesuaikan sistem baru yaitu AI System 
-          │Dan Merubah ulang Susunan Kode Ke System Yang Lebih Cepat
-          └$tnks"
+   [Exo] Add New Feature && Adjustment
+     ┌$new AOT Compiler
+     │Ini Adalah fitur compiler yang lebih optimal
+     ├$new AI System By Foxver Auto
+     │Ini adalah system ai yang di rancang untuk
+     │mengaktifkan auto render atau bisa di sebut
+     │mengubah render saat di dalam game Untuk aktivasi
+     │ada di changelogs
+     └┬$ads Menyesuaikan sistem baru yaitu AI System 
+      │Dan Merubah ulang Susunan Kode Ke System Yang Lebih Cepat
+      └$tnks"
