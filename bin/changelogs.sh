dd3acaecbc1984abc6b76382a46ebd7633fffe4a
Reiii3 <mohtoyib79@gmail.com>
2025-03-18 07:09:21 +0700

    update bin/changelogs.sh

--- bin/changelogs.sh
+++ bin/changelogs.sh
@@ -20,8 +20,8 @@ if [ $cek_beta = true ]; then
   echo "   ┌[Exo] New Update Beta Version"
   echo "   └┬$new AI System By Modules Foxver Auto"
   echo "    ├─$war Masih Dalam pengembangan, "
-  echo "    ├$adjust -System Modules"
-  echo "    │                -Render Selection"
+  echo "    └$adjust -System Modules"
+  echo "                     -Render Selection"
 else
   echo "Tesss"
 fi
